a19c72857b95c19755b2225ad1f4d5f37166b158
Gosh Darn Games <1109935+goshdarngames@users.noreply.github.com>
2019-06-05 13:07:55 +0100

    Rough skeleton code from Twitter Adventure Bot.

--- repeat_bot.py
+++ repeat_bot.py
@@ -5,3 +5,42 @@
 # exits.
 #############################################################################
 
+#----------------------------------------------------------------------------
+
+def post_image ( tc ):
+    pass
+
+#----------------------------------------------------------------------------
+
+def handle_exceptions ( exc_type, exc_value, exc_traceback ):
+    """
+    Used to log uncaught exceptions."
+    """
+
+    if issubclass ( exc_type, KeyboardInterrupt ):
+
+        sys.__excepthook__ ( exc_type, exc_value, exc_traceback )
+        return
+
+    logging.critical ( "Unhandled Exception", 
+                      exc_info = ( exc_type, exc_value, exc_traceback ) )
+#----------------------------------------------------------------------------
+
+def main ():
+
+    logging.basicConfig ( format='%(asctime)s - %(levelname)s - %(message)s', 
+                          level=logging.INFO )
+
+    sys.excepthook = handle_exceptions
+    
+    logging.info ( "Repeat Bot" )
+
+    logging.info ( "Creating Twitter Connection" )
+
+    with TwitterConnection () as tc:
+
+        post_image ( tc )
+
+
+if __name__ == "__main__":
+    sys.exit ( main () )
